df5f055154a02bfb798e096302e17a6ae3df14a1
Jeena A V <jeenalohi@gmail.com>
2024-02-28 23:30:36 +0100

    Initial commit

---
+++ popularRepo/src/main/java/com/challenge/popularRepo/PopularRepoApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.popularRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopularRepoApplication.class, args);
+	}
+
+}
---
+++ popularRepo/src/test/java/com/challenge/popularRepo/PopularRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.popularRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
